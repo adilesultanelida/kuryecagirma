89b6d18b7e2c5efa2ecbf1241d192cca94d73b6f
adilesultanelida <kerem.xcise@gmail.com>
2025-07-13 15:05:57 +0300

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -2,8 +2,6 @@ export default function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-
-  const fs = require('fs');
-  fs.writeFileSync('api/ping.json.js', JSON.stringify({ time: Date.now() }));
+  // Dosyaya yazma kısmı kaldırıldı
   res.status(200).json({ success: true });
 }
